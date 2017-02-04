09f38684dd9d54baef878f7843995e1770c18909
Ravibhushan Kumar <ravibhushankumarsonu@gmail.com>
2017-02-04 10:17:07 +0530

    Add files via upload

---
+++ Web Scraping/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeBehind="WebForm1.aspx.cs" Inherits="FebWeb.WebForms.WebForm1" Async="True" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Web Form</title>
+    <link href="../Content/bootstrap.css" rel="stylesheet" />
+    <script type="text/javascript" src="../scripts/jquery-1.9.1.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="text-center">
+    <h1>Working with Data Scraping</h1>
+        <div>
+            <p>
+                This page work with the data scraping with the C# using the HtmlAgilityPack nuget packet.<br />
+                
+            </p>
+        </div>
+    </div>
+        <asp:GridView ID="GridView1" cssClass="active center-block table table-hover" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Web Scraping/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+
+
+namespace FebWeb.WebForms
+{
+    public class NameAndScore
+    {
+        public string Name { get; set; }
+        public string Score { get; set; }
+    }
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DataTable tabel;
+        HtmlWeb web = new HtmlWeb();
+        public void InitTable()
+        {
+            tabel = new DataTable();
+            tabel.Columns.Add("Name", typeof(string));
+            tabel.Columns.Add("Score", typeof(string));
+            tabel.Rows.Add("Super Mario 64", "64%");
+        }
+
+        private async Task<List<NameAndScore>> GetRankingFromPage(int pageNumber)
+        {
+            string url = "http://www.gamerankings.com/browse.html";
+            if(pageNumber!=0)
+               url = "http://www.gamerankings.com/browse.html?page="+pageNumber.ToString();
+
+            var doc = await Task.Factory.StartNew(() => web.Load(url));
+            var nameNodes = doc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td//a");
+            var ScoreNodes = doc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td//span/b");
+            //Return if name and score nodes are null
+            if (nameNodes == null || ScoreNodes == null)
+                return new List<NameAndScore>();
+            var names = nameNodes.Select(node => node.InnerText);
+            var scores = ScoreNodes.Select(node => node.InnerText);
+            return names.Zip(scores, (name, score) => new NameAndScore() { Name = name, Score = score}).ToList();
+        }
+
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+            InitTable();
+           // int pageNum = 0;
+            var ranking = await GetRankingFromPage(0);
+            foreach (var item in ranking)
+            {
+                tabel.Rows.Add(item.Name, item.Score);
+            }
+            //This is used to load page for infinity time 
+            //while (ranking.Count>0)
+            //{
+            //    foreach (var item in ranking)
+            //    {
+            //        tabel.Rows.Add(item.Name, item.Score);
+            //    }
+            //    await GetRankingFromPage(++pageNum);
+            //}
+            GridView1.DataSource = tabel;
+            GridView1.DataBind();
+        }
+    }
+}
